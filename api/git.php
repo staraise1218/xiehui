44438440bfea651a787ee4e3514f872ef96aa08a
shengli <363912809@qq.com>
2019-06-05 22:53:05 +0800

    git

--- api/git.php
+++ api/git.php
@@ -7,4 +7,3 @@ defined('IN_PHPCMS') or exit('No permission resources.');
 $output = system("cd /home/www/xiehui; git pull 2>&1");
 echo "<pre>$output</pre>";
 
-file_put_contents('./log.txt', date('Y-m-d H:i:s'));
