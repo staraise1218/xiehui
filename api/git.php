b38340aa2678c200d411e16cabcf2fe23827b469
shengli <363912809@qq.com>
2019-06-05 17:23:24 +0800

    git

--- api/git.php
+++ api/git.php
@@ -4,6 +4,6 @@
  */
 defined('IN_PHPCMS') or exit('No permission resources.'); 
 
-$output = shell_exec("cd /home/www/xiehui; git pull 2<&1");
+$output = shell_exec("cd /home/www/xiehui; git pull ");
 var_dump($output);
 echo "<pre>$output</pre>";
