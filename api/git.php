fb84675ecead2408732b86fd8b72765dfcbb06f5
shengli <363912809@qq.com>
2019-06-05 17:22:06 +0800

    git

--- api/git.php
+++ api/git.php
@@ -3,6 +3,7 @@
  * 自动同步git
  */
 defined('IN_PHPCMS') or exit('No permission resources.'); 
-echo '1';
+
 $output = shell_exec("cd /home/www/xiehui; git pull 2<&1");
+var_dump($output);
 echo "<pre>$output</pre>";
