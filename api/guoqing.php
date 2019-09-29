391ebb658255d0569a9d00798817960eb7228e64
shengli <363912809@qq.com>
2019-09-29 17:15:12 +0800

    guoqing

--- api/guoqing.php
+++ api/guoqing.php
@@ -4,7 +4,9 @@ defined('IN_PHPCMS') or exit('No permission resources.');
  * 一起点亮中国文旅地图
  */
 
-$guoqing_db = pc_base::load_model('guoqing');
+$guoqing_model = pc_base::load_model('guoqing_model');
 
 $data = $_POST;
-p($data);
+
+$guoqing_model->insert($data);
+die(json_encode(array('code'=>200)));
