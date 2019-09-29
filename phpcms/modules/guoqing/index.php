bb3e7d76723d53c3145613e9ab3a1d345a0d7aa5
shengli <363912809@qq.com>
2019-09-29 12:09:52 +0800

    guoqing

---
+++ phpcms/modules/guoqing/index.php
@@ -0,0 +1,28 @@
+<?php
+defined('IN_PHPCMS') or exit('No permission resources.');
+//模型缓存路径
+define('CACHE_MODEL_PATH',CACHE_PATH.'caches_model'.DIRECTORY_SEPARATOR.'caches_data'.DIRECTORY_SEPARATOR);
+pc_base::load_app_func('util','content');
+class index {
+	private $db;
+	function __construct() {
+		$this->db = pc_base::load_model('content_model');
+		/*$this->_userid = param::get_cookie('_userid');
+		$this->_username = param::get_cookie('_username');
+		$this->_groupid = param::get_cookie('_groupid');*/
+	}
+
+	//首页
+	public function init() {
+		if(isset($_GET['siteid'])) {
+			$siteid = intval($_GET['siteid']);
+		} else {
+			$siteid = 1;
+		}
+		$siteid = $GLOBALS['siteid'] = max($siteid,1);
+		define('SITEID', $siteid);
+
+		include template('guoqing','index', 'mobile');
+	}
+}
+?>
